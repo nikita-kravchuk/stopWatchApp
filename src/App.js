942311efa45b37ab331790358bcd31221bb5d030
Nikita Kravchuk <nikita.kravchuk@ukr.net>
2021-08-25 17:26:46 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Subject, interval } from "rxjs";
+import { takeUntil } from "rxjs";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [watchOn, setWatchOn] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = new Subject();
+    interval(10)
+      .pipe(takeUntil(unsubscribe))
+      .subscribe(() => {
+        if (watchOn) {
+          setTime((val) => val + 1);
+        }
+      });
+    return () => {
+      unsubscribe.next();
+      unsubscribe.complete();
+    };
+  }, [watchOn]);
+
+  const handleStart = () => {
+    setWatchOn(prevState => !prevState);
+  }
+
+  const handleStop = () => {
+    if (time !== 0) {
+      setWatchOn(false);
+    }
+  }
+
+
+  const handleReset = () => {
+    setTime(0);
+    setWatchOn(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className='timeBlock'>
+          <span>
+            {("0" + Math.floor((time / (1000 * 60 * 60)) % 24)).slice(-2)}
+          </span>
+          &nbsp;:&nbsp;
+          <span>{("0" + Math.floor(time / 6000)).slice(-2)}</span>&nbsp;:&nbsp;
+          <span>{("0" + Math.floor((time / 100) % 60)).slice(-2)}</span>
+          &nbsp;:&nbsp;
+          <span>{("0" + Math.floor(time % 100)).slice(-2)}</span>
+        </div>
+        <div className="btnBlock">
+          <button onClick={handleStart}>Start/Stop</button>
+          <button onClick={handleStop}>Wait</button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
